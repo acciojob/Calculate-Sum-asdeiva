bde6b1d409018bbdcd7b005fd1023e256e69349d
DEIVA SIVAM <115927155+asdeiva@users.noreply.github.com>
2023-02-23 09:50:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,10 +3,9 @@ let n1=document.querySelector('#n1')
 let n2=document.querySelector('#n2')
 let sum=document.querySelector('#sum')
 function sumOf2(){
-
 	let n1 = parseFloat(n1.value);
 	let n2 = parseFloat(n2.value);
-
-	sum.innerHTML = n1+n2;
+	let result = n1+n2;
+	sum.innerHTML = result
 	
 }
