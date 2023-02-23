9c36cdc8f46d49eb6043993d3ff765e82783c9b4
DEIVA SIVAM <115927155+asdeiva@users.noreply.github.com>
2023-02-23 08:59:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+let n1=document.getElementById('n1').value
+let n2=document.getElementById('n2').value
+let sum=document.getElementById('sum')
+function sumOf2(){
+  console.log(parseInt(n1))
+  lett ans = parseInt(n1+n2)
+  if(ans!=0){
+    sum.innerHTML = parseInt(n1+n2)
+  }
+	
+}
